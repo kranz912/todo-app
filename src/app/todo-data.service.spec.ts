9e63a92e258bc3e5bbc60519d7b3d9ab3895883c
kranz fontanilla <me@kranznikolai.com>
2019-04-04 16:49:35 -0400

    Update todo-data.service.spec.ts

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 describe('TodoDataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -17,5 +18,15 @@ describe('#getAllTodos', ()=>{
       (service: TodoDataService)=>{
         expect(service.getAllTodos()).toEqual([]);
       })
+  );
+  it('should return all todos',
+  inject([TodoDataService],
+    (service:TodoDataService)=>{
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1,todo2]);
+    })
   );
 });
